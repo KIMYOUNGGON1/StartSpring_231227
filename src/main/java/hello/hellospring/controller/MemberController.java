89681b534b27d537d48b0c284facc12e14f9f6d1
KIMYOUNGGON1 <dudrhs950@naver.com>
2023-12-28 16:43:33 +0900

    20231228 수업내용

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -37,9 +37,9 @@ public class MemberController {
         return "redirect:/";
     }
 
-    @GetMapping("/members")
+    @GetMapping(value = "/members")
     public String list(Model model) {
-        List<Member> members = memberService.findMember();
+        List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
         return "members/memberList";
     }
